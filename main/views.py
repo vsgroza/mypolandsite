88c3522ece26abd8b0f5d97d6a18bec2bbb7b72b
vsgroza <vsgroza@gmail.com>
2024-08-31 11:52:09 +0300

    unitcommit

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from lib2to3.fixes.fix_input import context
+from unicodedata import category
 
 from django.shortcuts import render
 from .models import Category, Post
@@ -7,8 +7,21 @@ from .models import Category, Post
 
 def index(request):
     posts = Post.objects.all()
+    categories = Category.objects.all()
     context = {
         'title': 'Главная страница',
-        'posts': posts
+        'posts': posts,
+        'categories': categories,
+    }
+    return render(request, 'main/index.html', context)
+
+def category_list(request):
+    """Реацкция на нажатие кнопки категории"""
+    posts = Post.objects.filter(category_id=pk)
+    categories = Category.objects.all()
+    context = {
+        'title': posts[0].category,
+        'posts': posts,
+        'categories': categories,
     }
     return render(request, 'main/index.html', context)
