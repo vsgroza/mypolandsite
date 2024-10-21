367d6776d79016e60904bb2766f02dfcc0c4dfc5
vsgroza <vsgroza@gmail.com>
2024-10-21 17:04:53 +0300

    unitcommit

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,15 @@ from .views import *
 urlpatterns = [
     # path('', index, name='index'),
     path('', Index.as_view(), name='index'),
-    # path('category/<str:pk>', category_list, name='category_list'),
+    # path('category/<int:pk>', category_list, name='category_list'),
     path('category/<int:pk>/', ArticByCategory.as_view(), name='category_list'),
-    path('post/<int:pk>', post_detail, name='post_detail'),
-    path('add_article/', add_post, name='add'),
+    # path('post/<int:pk>', post_detail, name='post_detail'),
+    path('post/<int:pk>', PostDetail.as_view(), name='post_detail'),
+    # path('add_article/', add_post, name='add'),
+    path('add_article/', AddPost.as_view(), name='add'),
+    path('post/<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
+
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('register/', register, name='register'),
--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from .forms import PostAddForm, LoginForm, RegisterForm
 from django.contrib.auth import login, logout
 from django.contrib import messages
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 # def index(request):
@@ -25,6 +26,7 @@ class Index(ListView):
     context_object_name = 'posts'
     template_name = 'main/index.html'
     extra_context = {'title': 'Главная страница',}
+
 
 # def category_list(request, pk):
 #     """Реацкция на нажатие кнопки категории"""
@@ -52,37 +54,79 @@ class ArticByCategory(ListView):
         return context
 
 
-def post_detail(request, pk):
-    """Странички статьи"""
-    article = Post.objects.get(pk=pk)
-    Post.objects.filter(pk=pk).update(watched=F('watched') + 1)
-    ext_post = Post.objects.all().exclude(pk=pk).order_by('-watched')
-    context = {
-        'title': article,
-        'post': article,
-        'ext_post': ext_post,
-    }
-    return render(request, 'main/article_detail.html', context)
+
+# def post_detail(request, pk):
+#     """Странички статьи"""
+#     article = Post.objects.get(pk=pk)
+#     Post.objects.filter(pk=pk).update(watched=F('watched') + 1)
+#     ext_post = Post.objects.all().exclude(pk=pk).order_by('-watched')
+#     context = {
+#         'title': article,
+#         'post': article,
+#         'ext_post': ext_post,
+#     }
+#     return render(request, 'main/article_detail.html', context)
+
+class PostDetail(DetailView):
+
+    template_name = 'main/article_detail.html'
+
+    def get_queryset(self):
+        """Здесь дополнительная фильтрация"""
+        return Post.objects.filter(pk=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        """Для динамических данных"""
+        Post.objects.filter(pk=self.kwargs['pk']).update(watched=F('watched')+1)
+        context = super().get_context_data()
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        posts = Post.objects.all().exclude(pk=self.kwargs['pk']).order_by('-watched')[:5]
+        context['title'] = post.title
+        context['ext_posts'] = posts
+        return context
+
+# def add_post(request):
+#     """Добавление статьи от пользователя, без админки"""
+#     if request.method == 'POST':
+#         form = PostAddForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             post = Post.objects.create(**form.cleaned_data)
+#             post.save()
+#             return redirect('post_detail', post.pk)
+#
+#     else:
+#         form = PostAddForm()
+#
+#     context = {
+#         'form': form,
+#         'title': 'Добавить статью'
+#     }
+#
+#     return render(request, 'main/article_add_form.html', context)
+
+class AddPost(CreateView):
+    """Добавление статьи без админки"""
+    form_class = PostAddForm
+    template_name = 'main/article_add_form.html'
+    extra_context = {'title': 'Добавить статью'}
 
 
-def add_post(request):
-    """Добавление статьи от пользователя, без админки"""
-    if request.method == 'POST':
-        form = PostAddForm(request.POST, request.FILES)
-        if form.is_valid():
-            post = Post.objects.create(**form.cleaned_data)
-            post.save()
-            return redirect('post_detail', post.pk)
 
-    else:
-        form = PostAddForm()
+class PostUpdate(UpdateView):
+    """Изменение статьи"""
+    model = Post
+    form_class = PostAddForm
+    template_name = 'main/article_add_form.html'
+    extra_context = {'title': 'Изменить статью'}
 
-    context = {
-        'form': form,
-        'title': 'Добавить статью'
-    }
 
-    return render(request, 'main/article_add_form.html', context)
+class PostDelete(DeleteView):
+    """Удаление статьи"""
+    model = Post
+    success_url = reverse_lazy('index')
+    context_object_name = 'post'
+    extra_context = {'title': 'Удаление статьи'}
+
 
 
 def user_login(request):
